99c3d33d79b09dc7996f38ce88c00bcc22486d04
Tomas Laurinavicius <hi@tomaslau.com>
2023-12-21 14:40:21 +0100

    Updated repo base URL

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 const config: DocsThemeConfig = {
   logo: <span>Cheat Sheets</span>,
   project: {
-    link: "https://github.com/craftled/cheatsheets/",
+    link: "https://github.com/craftled/cheatsheets",
   },
   chat: {
     link: "https://discord.com",
   },
-  docsRepositoryBase: "https://github.com/craftled/cheatsheets/",
+  docsRepositoryBase: "https://github.com/craftled/cheatsheets/tree/main",
   footer: {
     text: "2023-2024 © Craftled Cheat Sheets. Technical cheat sheets for developers. Standing on the shoulders of giants.",
   },
