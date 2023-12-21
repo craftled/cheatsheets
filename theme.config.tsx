154a3b5d68c0bb222b49902b261064c93edc38f8
Tomas Laurinavicius <hi@tomaslau.com>
2023-12-21 14:21:57 +0100

    Switched to npm from pnpm

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Cheat Sheets</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/craftled/cheatsheets",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/craftled/cheatsheets",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "2023-2024 © Craftled Cheat Sheets. Technical cheat sheets for developers. Standing on the shoulders of giants.",
   },
-}
+};
 
-export default config
+export default config;
